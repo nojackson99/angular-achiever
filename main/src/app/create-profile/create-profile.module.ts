3a12b8bcb089358ad7a09985a94a46de2a6e7f76
Noah Jackson <nojackson99@gmail.com>
2022-11-04 14:40:52 -0400

    form complete with validation - values ready to be saved

--- main/src/app/create-profile/create-profile.module.ts
+++ main/src/app/create-profile/create-profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,8 +13,10 @@ import { CreateProfilePage } from './create-profile.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    CreateProfilePageRoutingModule
+    CreateProfilePageRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [CreateProfilePage]
 })
-export class CreateProfilePageModule {}
+export class CreateProfilePageModule { }
--- main/src/app/create-profile/create-profile.page.ts
+++ main/src/app/create-profile/create-profile.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'app-create-profile',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-profile.page.scss'],
 })
 export class CreateProfilePage implements OnInit {
+  isSubmitted = false;
+  createProfileForm: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    this.createProfileForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.createProfileForm.valid) {
+      console.log("Please provide all required inputs")
+      return false;
+    } else {
+      console.log(this.createProfileForm.value)
+    }
+  }
+
+  get errorControl() {
+    return this.createProfileForm.controls;
+  }
 }
